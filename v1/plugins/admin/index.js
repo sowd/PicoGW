2b9d2a91a8ce3acd3814109d0d02418ee39f6b28
Shigeru Owada <sowd@acm.org>
2017-08-11 17:29:19 +0000

    port change #1

--- v1/plugins/admin/index.js
+++ v1/plugins/admin/index.js
@@ -42,183 +42,192 @@ exports.init = function(pi){
 
 	pluginInterface.setOnGetSettingsSchemaCallback( function(){
    		return new Promise((ac,rj)=>{
+		  try {
+			let schema_json = JSON.parse(fs.readFileSync(pluginInterface.getpath()+'settings_schema.json').toString()) ;
+			let schema_default_json = JSON.parse(fs.readFileSync(pluginInterface.getpath()+'settings_schema_default.json').toString()) ;
+			let schema_wlan_json = JSON.parse(fs.readFileSync(pluginInterface.getpath()+'settings_schema_wlan.json').toString()) ;
+
 			exec('nmcli d', (err, stdout, stderr) => {
-			  let lines = stdout.split("\n") ;
-			  if( err || lines.length<2 ){
-			  	ac({error:'nmcli should be installed first. Execute\n\n'
-			  		+'$ sudo apt-get install network-manager\n\nor\n\n$ sudo yum install NetworkManager\n\n'
-			  		+'Also, you need to free your network devices from existing framework by, for example, edit /etc/network/interfaces to contain only two lines:\n\n'
-			  		+'auto lo\n'
-			  		+'iface lo inet loopback\n\n'
-			  		+'and reboot. In addition, you may want to uninstall dhcpcd5 (if exist) by\n\n'
-			  		+'$ sudo apt-get purge dhcpcd5'
-			  	}) ;
-			  	return ;
-			  }
-			  lines.shift() ;
-			  if( lines.length==0 ){ ac({error:'No network available.'}) ; return ; }
+				let lines = stdout.split("\n") ;
+				if( err || lines.length<2 ){
+				  	delete schema_json.properties.interfaces ;
+				  	delete schema_json.properties.detail ;
+				  	delete schema_json.properties.root_passwd ;
+				  	schema_json.properties.network_settings = {
+				  		type:'object'
+				  		,description: 'nmcli should be installed to setup network configuration. Execute\n\n'
+				  		+'"$ sudo apt-get install network-manager"\n\nor\n\n"$ sudo yum install NetworkManager"\n\n'
+				  		+'Also, you need to free your network devices from existing framework by, for example, edit /etc/network/interfaces to contain only two lines:\n\n'
+				  		+'"auto lo",\n'
+				  		+'"iface lo inet loopback"\n\n'
+				  		+'and reboot. In addition, you may want to uninstall dhcpcd5 (if exist) by\n\n'
+				  		+'"$ sudo apt-get purge dhcpcd5"'
+				  	} ;
+				  	ac( schema_json ) ;
+				} else {
+					lines.shift() ;
+					if( lines.length==0 ){ ac({error:'No network available.'}) ; return ; }
 
-			  try {
-				let schema_json = JSON.parse(fs.readFileSync(pluginInterface.getpath()+'settings_schema.json').toString()) ;
-				let schema_default_json = JSON.parse(fs.readFileSync(pluginInterface.getpath()+'settings_schema_default.json').toString()) ;
-				let schema_wlan_json = JSON.parse(fs.readFileSync(pluginInterface.getpath()+'settings_schema_wlan.json').toString()) ;
-
-				let cur_settings_interf , cur_settings_ap ;
-				try {
-					let cur_settings = JSON.parse(fs.readFileSync(pluginInterface.getpath()+'settings.json').toString()) ;
-					for( let k in cur_settings.interfaces ){
-						cur_settings_interf = k ;
-						if( k.indexOf('wlan')==0 )
-							cur_settings_ap = cur_settings.interfaces[k].apname ;
-					}
-				} catch(e){} ;
-				// Correct visible APs should be listed
-				let bWlanExist = false ;
-				let interfaces = [] ;
-				lines.forEach( line=>{
-				  	let sp = line.trim().split(/\s+/) ;
-				  	if( sp.length !=4 || sp[0]=='lo') return ;	// Illegally formatted line
-					if( sp[0].indexOf('wlan')==0 ) bWlanExist = true ;
-				  	interfaces.push(sp[0]) ;
-				}) ;
-
-				// Previously set interface does not exist any more..
-				if( cur_settings_interf !=undefined && interfaces.indexOf[cur_settings_interf]<0){
-					log(`Previously set interface ${cur_settings_interf} does not exist now`) ;
-					interfaces.unshift(cur_settings_interf) ;
-				}
-
-				interfaces.forEach( interf=>{
-			  		let prop = {} ;
-
-					prop[interf] = ( interf.indexOf('wlan')==0 ? schema_wlan_json : schema_default_json ) ;
-
-				  	schema_json.properties.interfaces.oneOf.push({
-				  		title:interf
-				  		,type:'object'
-				  		,additionalProperties: false
-				  		,properties:prop
-				  	}) ;
-				}) ;
-
-				if( schema_json.properties.interfaces.oneOf.length==0){ ac({error:'No network available.'}) ; return ; }
-
-				if( !bWlanExist ){
-			   		ac( schema_json ) ;
-					//log(JSON.stringify(schema_json,null,'\t')) ;
-				} else { // WiFi scan
-					exec('nmcli dev wifi list', (err, stdout, stderr) => {
-						if( err )	log('Cannot scan Wifi APs (possibly because "nmcli dev wifi list" command requires sudo?)') ;
-						let lines = stdout.split("\n") ;
-						lines.shift() ;
-						lines.forEach( line=>{
-							let li = line.indexOf('Infra') ;
-							if( li==-1 ) li = line.indexOf('インフラ') ;
-							if( li==-1 ) return ;
-						  	let sp = line.slice(0,li).trim() ;
-						  	if( sp[0]=='*') sp = sp.slice(1).trim() ;
-						  	if( sp == '--' ) return ;
-							schema_wlan_json.properties.apname.enum.push(sp) ;
-						} ) ;
-
-						if( schema_wlan_json.properties.apname.enum.length == 0 )
-							log('No valid Wifi IP found.') ;
-
-						if( cur_settings_ap !=undefined && schema_wlan_json.properties.apname.enum.indexOf[cur_settings_ap]<0){
-							log(`Previously set AP ${cur_settings_ap} is invisible now`) ;
-							schema_wlan_json.properties.apname.enum.unshift(cur_settings_ap) ;
+					let cur_settings_interf , cur_settings_ap ;
+					try {
+						let cur_settings = JSON.parse(fs.readFileSync(pluginInterface.getpath()+'settings.json').toString()) ;
+						for( let k in cur_settings.interfaces ){
+							cur_settings_interf = k ;
+							if( k.indexOf('wlan')==0 )
+								cur_settings_ap = cur_settings.interfaces[k].apname ;
 						}
-
-						ac( schema_json ) ;
-						//log(JSON.stringify(schema_json,null,'\t')) ;
+					} catch(e){} ;
+					// Correct visible APs should be listed
+					let bWlanExist = false ;
+					let interfaces = [] ;
+					lines.forEach( line=>{
+					  	let sp = line.trim().split(/\s+/) ;
+					  	if( sp.length !=4 || sp[0]=='lo') return ;	// Illegally formatted line
+						if( sp[0].indexOf('wlan')==0 ) bWlanExist = true ;
+					  	interfaces.push(sp[0]) ;
 					}) ;
+
+					// Previously set interface does not exist any more..
+					if( cur_settings_interf !=undefined && interfaces.indexOf[cur_settings_interf]<0){
+						log(`Previously set interface ${cur_settings_interf} does not exist now`) ;
+						interfaces.unshift(cur_settings_interf) ;
+					}
+
+					interfaces.forEach( interf=>{
+				  		let prop = {} ;
+
+						prop[interf] = ( interf.indexOf('wlan')==0 ? schema_wlan_json : schema_default_json ) ;
+
+					  	schema_json.properties.interfaces.oneOf.push({
+					  		title:interf
+					  		,type:'object'
+					  		,additionalProperties: false
+					  		,properties:prop
+					  	}) ;
+					}) ;
+
+					if( schema_json.properties.interfaces.oneOf.length==0){ ac({error:'No network available.'}) ; return ; }
+
+					if( !bWlanExist ){
+				   		ac( schema_json ) ;
+						//log(JSON.stringify(schema_json,null,'\t')) ;
+					} else { // WiFi scan
+						exec('nmcli dev wifi list', (err, stdout, stderr) => {
+							if( err )	log('Cannot scan Wifi APs (possibly because "nmcli dev wifi list" command requires sudo?)') ;
+							let lines = stdout.split("\n") ;
+							lines.shift() ;
+							lines.forEach( line=>{
+								let li = line.indexOf('Infra') ;
+								if( li==-1 ) li = line.indexOf('インフラ') ;
+								if( li==-1 ) return ;
+							  	let sp = line.slice(0,li).trim() ;
+							  	if( sp[0]=='*') sp = sp.slice(1).trim() ;
+							  	if( sp == '--' ) return ;
+								schema_wlan_json.properties.apname.enum.push(sp) ;
+							} ) ;
+
+							if( schema_wlan_json.properties.apname.enum.length == 0 )
+								log('No valid Wifi IP found.') ;
+
+							if( cur_settings_ap !=undefined && schema_wlan_json.properties.apname.enum.indexOf[cur_settings_ap]<0){
+								log(`Previously set AP ${cur_settings_ap} is invisible now`) ;
+								schema_wlan_json.properties.apname.enum.unshift(cur_settings_ap) ;
+							}
+
+							ac( schema_json ) ;
+							//log(JSON.stringify(schema_json,null,'\t')) ;
+						}) ;
+					}
 				}
-
-
-		   	  } catch(e){ac({error:'Cannot read admin/settings_schema*.json'});} 
 			});
+	   	  } catch(e){ac({error:'Cannot read admin/settings_schema*.json'});}
 
 		}) ;
 	}) ;
 
 	pluginInterface.setOnSettingsUpdatedCallback( function(newSettings){
 		return new Promise((ac,rj)=>{
-			let root_pwd = newSettings.root_passwd ;
-			newSettings.root_passwd = '' ;	// Root password is not saved to the file
-			//ac();return;
-			//log('NewSettings:') ;
-			//log(JSON.stringify(newSettings,null,'\t')) ;
+			if( newSettings.server_port != -1 )
+				pluginInterface.publish('client_settings',{port:newSettings.server_port}) ;
 
-			let interf ;
-			for( let k in newSettings.interfaces )
-				interf = k ;
-			let ss = newSettings.interfaces[interf] ;
+			if( newSettings.interfaces != null ){
+				let root_pwd = newSettings.root_passwd ;
+				newSettings.root_passwd = '' ;	// Root password is not saved to the file
+				//ac();return;
+				//log('NewSettings:') ;
+				//log(JSON.stringify(newSettings,null,'\t')) ;
 
-			const NMCLI_CONNECTION_NAME = NMCLI_CONNECTION_NAME_PREFIX + '_' + interf ;
-			let commands = [] ;
-			// Delete connection (may fail for first time)
-			commands.push(['nmcli','connection','down',NMCLI_CONNECTION_NAME]) ;
-			commands.push(['nmcli','connection','delete',NMCLI_CONNECTION_NAME]) ;
+				let interf ;
+				for( let k in newSettings.interfaces )
+					interf = k ;
+				let ss = newSettings.interfaces[interf] ;
 
-			if( interf.indexOf('wlan')==0 ){
-				commands.push(['nmcli','connection','add','con-name',NMCLI_CONNECTION_NAME
-					,'type','wifi','ifname', interf, 'ssid'
-					,(ss.apname_manual.trim().length==0 ? ss.apname : ss.apname_manual.trim())]) ;
-			} else if( interf.indexOf('eth')==0 )
-				commands.push(['nmcli','connection','add','con-name',NMCLI_CONNECTION_NAME
-				 ,'type','ethernet','ifname', interf]) ;
+				const NMCLI_CONNECTION_NAME = NMCLI_CONNECTION_NAME_PREFIX + '_' + interf ;
+				let commands = [] ;
+				// Delete connection (may fail for first time)
+				commands.push(['nmcli','connection','down',NMCLI_CONNECTION_NAME]) ;
+				commands.push(['nmcli','connection','delete',NMCLI_CONNECTION_NAME]) ;
+
+				if( interf.indexOf('wlan')==0 ){
+					commands.push(['nmcli','connection','add','con-name',NMCLI_CONNECTION_NAME
+						,'type','wifi','ifname', interf, 'ssid'
+						,(ss.apname_manual.trim().length==0 ? ss.apname : ss.apname_manual.trim())]) ;
+				} else if( interf.indexOf('eth')==0 )
+					commands.push(['nmcli','connection','add','con-name',NMCLI_CONNECTION_NAME
+					 ,'type','ethernet','ifname', interf]) ;
 
 
-			if( newSettings.detail.ip == undefined ){	// DHCP
-				commands.push(['nmcli','connection','modify',NMCLI_CONNECTION_NAME
-				 ,'ipv4.method','auto']) ;
-			} else {	// static ip
-				if( newSettings.detail.default_gateway == undefined )	newSettings.detail.default_gateway = '' ;
-				let ipSetting = (newSettings.detail.ip+' '+newSettings.detail.default_gateway).trim() ;
-				commands.push(['nmcli','connection','modify',NMCLI_CONNECTION_NAME
-					,'ipv4.method','manual','ipv4.addresses',ipSetting]) ;
-			}
-
-			if( interf.indexOf('wlan')==0 ){
-				if( ss.password != ss.password2 ){
-					rj('Password mismatch.') ;
-					return ;
+				if( newSettings.detail.ip == undefined ){	// DHCP
+					commands.push(['nmcli','connection','modify',NMCLI_CONNECTION_NAME
+					 ,'ipv4.method','auto']) ;
+				} else {	// static ip
+					if( newSettings.detail.default_gateway == undefined )	newSettings.detail.default_gateway = '' ;
+					let ipSetting = (newSettings.detail.ip+' '+newSettings.detail.default_gateway).trim() ;
+					commands.push(['nmcli','connection','modify',NMCLI_CONNECTION_NAME
+						,'ipv4.method','manual','ipv4.addresses',ipSetting]) ;
 				}
-				const ap_pwd = ss.password ; ss.password = ss.password2 = '' ;
-				commands.push(['nmcli','connection','modify',NMCLI_CONNECTION_NAME
-					,'wifi-sec.key-mgmt','wpa-psk','wifi-sec.psk',ap_pwd]) ;
-			}
-			//commands.push(['nmcli','connection','down', NMCLI_CONNECTION_NAME]) ;
-			commands.push(['nmcli','connection','up'  , NMCLI_CONNECTION_NAME]) ;
 
-			//log('Commands:') ;
-			//log(JSON.stringify(commands,null,'\t')) ;
-
-			const ignore_error_cmds = ['delete','down' /*,'up'*/] ;
-			function ex(){
-				if( commands.length==0 ){
-					ipv4.refreshMyAddress() ;
-					ac() ;
-					return ;
+				if( interf.indexOf('wlan')==0 ){
+					if( ss.password != ss.password2 ){
+						rj('Password mismatch.') ;
+						return ;
+					}
+					const ap_pwd = ss.password ; ss.password = ss.password2 = '' ;
+					commands.push(['nmcli','connection','modify',NMCLI_CONNECTION_NAME
+						,'wifi-sec.key-mgmt','wpa-psk','wifi-sec.psk',ap_pwd]) ;
 				}
-				let cmd = commands.shift() ;
-				//log('Exec:'+cmd.join(" ")) ;
-				let child = sudo(cmd,{password:root_pwd}) ;
-				child.stderr.on('data',dat=>{
-					console.error('Error in executing\n$ '+cmd.join(' ')+'\n'+dat.toString()) ;
-					if( ignore_error_cmds.indexOf(cmd[2]) >= 0 ) return ;
-					rj('Error in executing\n\n$ '+cmd.join(' ')+'\n\n'+dat.toString()) ;	// Interrupt execution
-					commands = [] ;
-				}) ;
-				child.stdout.on('close',()=>{
-					if( commands.length == 0 ){
+				//commands.push(['nmcli','connection','down', NMCLI_CONNECTION_NAME]) ;
+				commands.push(['nmcli','connection','up'  , NMCLI_CONNECTION_NAME]) ;
+
+				//log('Commands:') ;
+				//log(JSON.stringify(commands,null,'\t')) ;
+
+				const ignore_error_cmds = ['delete','down' /*,'up'*/] ;
+				function ex(){
+					if( commands.length==0 ){
 						ipv4.refreshMyAddress() ;
 						ac() ;
 						return ;
-					} else ex() ;
-				}) ;
+					}
+					let cmd = commands.shift() ;
+					//log('Exec:'+cmd.join(" ")) ;
+					let child = sudo(cmd,{password:root_pwd}) ;
+					child.stderr.on('data',dat=>{
+						console.error('Error in executing\n$ '+cmd.join(' ')+'\n'+dat.toString()) ;
+						if( ignore_error_cmds.indexOf(cmd[2]) >= 0 ) return ;
+						rj('Error in executing\n\n$ '+cmd.join(' ')+'\n\n'+dat.toString()) ;	// Interrupt execution
+						commands = [] ;
+					}) ;
+					child.stdout.on('close',()=>{
+						if( commands.length == 0 ){
+							ipv4.refreshMyAddress() ;
+							ac() ;
+							return ;
+						} else ex() ;
+					}) ;
+				}
+				ex() ;
 			}
-			ex() ;
 		}) ;
 	}) ;
 
